ee84742a227b1ff8e6210b718ca38e66f9420890
unknown <you@example.com>
2022-06-24 22:42:40 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,32 @@
-import path from 'path'
-import express from  'express'
+// import path from 'path'
+// import express from  'express'
 // import products from  './data/products.js'
-import dotenv from  'dotenv'
-import connectDB from './config/db.js'
-import  Color  from 'colors'
-import productRouter from './routes/productRoutes.js'
-import { notFound,errorHandler } from './middleware/errorMiddleware.js'
-import  authUserRouter  from './routes/userRoutes.js'
-import  orderRouter  from './routes/orderRoutes.js'
-import  uploadRouter  from './routes/uploadRoutes.js'
-import morgan from 'morgan'
-dotenv.config()
-connectDB()
-const app=express()
+// import dotenv from  'dotenv'
+// import connectDB from './config/db.js'
+// import  Color  from 'colors'
+// import productRouter from './routes/productRoutes.js'
+// import { notFound,errorHandler } from './middleware/errorMiddleware.js'
+// import  authUserRouter  from './routes/userRoutes.js'
+// import  orderRouter  from './routes/orderRoutes.js'
+// import  uploadRouter  from './routes/uploadRoutes.js'
+// import morgan from 'morgan'
+// dotenv.config()
+// connectDB()
+// const app=express()
 
-if(process.env.NODE_ENV ==='development'){
-    app.use(morgan('dev'))
-}
+// if(process.env.NODE_ENV ==='development'){
+//     app.use(morgan('dev'))
+// }
 
 
-app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send('Api is running')
-})
+// app.use(express.json())
+// app.get('/',(req,res)=>{
+//     res.send('Api is running')
+// })
+// end
+
+
+
 
 // app.use((req,res,next)=>{
 //     console.log('hello');
@@ -30,32 +34,42 @@ app.get('/',(req,res)=>{
 //     next()
 // })
 
-app.use('/products' , productRouter)
-app.use('/users' , authUserRouter)
-app.use('/orders' , orderRouter)
-app.use('/upload',uploadRouter)
 
-app.get('/api/config/paypal',(req,res)=>
-res.send(process.env.PAYPAL_CLIENT_ID)
-)
-const __dirname=path.resolve()
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+// new
+
+// app.use('/products' , productRouter)
+// app.use('/users' , authUserRouter)
+// app.use('/orders' , orderRouter)
+// app.use('/upload',uploadRouter)
+
+// app.get('/api/config/paypal',(req,res)=>
+// res.send(process.env.PAYPAL_CLIENT_ID)
+// )
+// const __dirname=path.resolve()
+// app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')))
   
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    )
-  } else {
-    app.get('/', (req, res) => {
-      res.send(`API is running....`)
+//     app.get('*', (req, res) =>
+//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//     )
+//   } else {
+//     app.get('/', (req, res) => {
+//       res.send(`API is running....`)
      
-    })
-  }
+//     })
+//   }
 
-app.use(notFound)
-app.use(errorHandler)
+// app.use(notFound)
+// app.use(errorHandler)
+// end
+
+
+
+
 
 // app.use((req,res,next)=>{
 //     const error=  new Error (`Not Found ${req.originalUrl}`)
@@ -81,5 +95,83 @@ app.use(errorHandler)
 //      res.json(product)
 //  })
 
-const PORT=process.env.PORT || 5000
- app.listen(PORT,console.log(`app is running in ${process.env.NODE_ENV} ON ${PORT}`.yellow.bold))
+
+// new
+
+// const PORT=process.env.PORT || 5000
+//  app.listen(PORT,console.log(`app is running in ${process.env.NODE_ENV} ON ${PORT}`.yellow.bold))
+
+// end
+
+
+
+
+
+
+
+
+
+
+
+
+
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+
+app.use('/products', productRoutes)
+app.use('/users', userRoutes)
+app.use('/orders', orderRoutes)
+app.use('/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
