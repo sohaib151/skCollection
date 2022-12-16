f6cdfe92e1b1855864d1e0d275fc0228e980057a
Your Name <you@example.com>
2022-12-16 22:57:42 +0000

    first co

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import path from 'path'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -11,6 +10,7 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
+import path from 'path'
 
 dotenv.config()
 
@@ -18,9 +18,9 @@ connectDB()
 
 const app = express()
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'))
+// }
 
 app.use(express.json())
 
@@ -34,7 +34,7 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 const __dirname = path.resolve()
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '/frontend/build')))
@@ -50,11 +50,10 @@ const __dirname = path.resolve()
 
   app.use(express.static(path.join(__dirname,'./frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
-  )
-  //  const sk= path.join(__dirname,'./frontend/build')
-  //  console.log(sk);
+  app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
+  })
+  
 app.use(notFound)
 app.use(errorHandler)
 
